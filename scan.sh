3729a2a809fc769bc60fa35f4da482a0bb58b0c4
scarret <luislopez188@hotmail.com>
2020-03-24 13:11:40 -0600

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/bash
-#Envia una peticion TCP a los 65535 puertos, si el puerto se encuentra cerrado retorna un error, si no llegase a retornar el error se imprime en pantalla que
-#el puerto se encuentra abierto
+#Autor: Luis Carreto
+#Descripcion: Envia una peticion TCP a los 65535 puertos, si el puerto se encuentra cerrado retorna un error, si no llegase a retornar el error se imprime en pantalla que el puerto se encuentra abierto
 
 for port in $(seq 1 65535); do
-        timeout 1 bash -c "echo > /dev/tcp/$1/$port" 2>/dev/null && echo "PORT $port OPEN " &
+        timeout 1 bash -c "echo > /dev/tcp/$1/$port" 2>/dev/null && echo "PORT $port OPEN " && echo "PORT $port OPEN " >> scan &
 done; wait
+
