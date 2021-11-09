6779e2ad8d516e2df47d42f286396bf16f9e605c
scarret <luislopez188@hotmail.com>
2021-11-08 22:48:23 -0600

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -8,16 +8,16 @@ if [ "$1" == "" ]; then
           exit 0                                                                                               
 fi                                                                                                                                                                                                                        
 echo
-echo "[+]INTENTANDO IDENTIFICAR EL S.O EN BASE AL TTL "
+echo "[+]INTENTANDO IDENTIFICAR EL S.O EN BASE AL TTL " && echo "[+]INTENTANDO IDENTIFICAR EL S.O EN BASE AL TTL " >> scanports_$1 &
 ttl=$(ping -c 1 $1 | grep ttl | sed 's/.*\sttl=\(...\).*/\1/')
   if [[ $ttl -gt 120 && $ttl -le 128  ]];then
-          echo "[!] ==POSIBLE SISTEMA WINDOWS=="; echo;
+          echo "[!] ==POSIBLE SISTEMA WINDOWS==" && echo "[!] ==POSIBLE SISTEMA WINDOWS==" >> scanports_$1
   elif [[ $ttl -gt 60 && $ttl -le 64 ]];then
-          echo "[!] ==POSIBLE SISTEMA LINUX=="; echo;
+          echo "[!] ==POSIBLE SISTEMA LINUX==" && echo "[!] ==POSIBLE SISTEMA LINUX==" >> scanports_$1
   elif [ $ttl == "" ];then
-          echo "[-] SIN RESPUESTA, POSIBLE HOST DOWN O BLOQUEO ICMP!!"; echo;        
+          echo "[-] SIN RESPUESTA, POSIBLE HOST DOWN O BLOQUEO ICMP!!" && echo "[!] ==POSIBLE SISTEMA LINUX==" >> scanports_$1        
   else
-          echo "[-] NO FUE POSIBLE IDENTIFICAR EL SISTEMA TTL DE $ttl"; echo;     
+          echo "[-] NO FUE POSIBLE IDENTIFICAR EL SISTEMA TTL DE $ttl" && echo "[-] NO FUE POSIBLE IDENTIFICAR EL SISTEMA TTL DE $ttl" >> scanports_$1     
 fi
 echo "[+]Starting scan ports " 
 echo "[+]Scan in $1" && echo "[+]Scan in $1" > scanports_$1
@@ -25,3 +25,4 @@ for port in $(seq 1 65535); do
 
         timeout 1 bash -c "echo > /dev/tcp/$1/$port" 2>/dev/null && echo "PORT $port OPEN " && echo "PORT $port OPEN " >> scanports_$1 &
 done; wait
+
